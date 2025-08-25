003deb2bdd295f800503534b69915ae53d11ddb6
Persii <tomiipersii@gmail.com>
2025-08-25 19:34:45 -0300

    Ejercicio 2.3 Done

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,9 @@ const Part = ({ name, exercises }) => (
 
 const Content = ({ parts }) => (
   <div>
-
-    <Part name={parts[0].name} exercises={parts[0].exercises} />
-    <Part name={parts[1].name} exercises={parts[1].exercises} />
-    <Part name={parts[2].name} exercises={parts[2].exercises} />
+    {parts.map(part => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
   </div>
 )
 const Total = ({ parts }) => {
