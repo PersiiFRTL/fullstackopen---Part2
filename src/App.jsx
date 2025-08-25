d4f129c7307b5023e3804d7cb88b1f20b75bb9f1
Persii <tomiipersii@gmail.com>
2025-08-25 20:04:34 -0300

    Ejercicio 2.5 Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} name={part.name} exercises={part.exercises} />
-    ))}
-  </div>
-);
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p>
-      <b>Total of {total} exercises</b>
-    </p>
-  );
-};
-
-const Course = ({ course }) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-);
+import Course from "./Course";
 
 const App = () => {
   const courses = [
