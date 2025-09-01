b5729cf69beb294335f3e46cfe05c6664880dac5
Persii <tomiipersii@gmail.com>
2025-08-31 22:35:59 -0300

    Ejercicio 2.15 Done

--- src/App.jsx
+++ src/App.jsx
@@ -10,77 +10,68 @@ import Notification from './Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [notification, setNotification] = useState(null)
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/persons')
+      .get("http://localhost:3001/persons")
       .then(response => {
-        setPersons(response.data)
+        setPersons(response.data)               // cargar datos iniciales al backend
       })
   }, [])
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)              // manejar cambio del nombre
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)            // manejar cambio del numero  
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)              // manejar cambio del filtro
+  }
+
   const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
+    event.preventDefault()                      // agregar nueva persona 
+
+    const newPerson = {
       name: newName,
       number: newNumber
     }
 
-    const existingPerson = persons.find(p => p.name === newName)
-
-    if (existingPerson) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    }
-
     axios
-      .post('http://localhost:3001/persons', personObject)
+      .post("http://localhost:3001/persons", newPerson)
       .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-        setNotification(`Added ${response.data.name}`)
-        setTimeout(() => {
-          setNotification(null)
-        }, 3000)
+        setPersons(persons.concat(response.data))       // POST al backend
+        setNewName("")
+        setNewNumber("")
       })
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const personsToShow = persons.filter(person =>        // aplicar filtro
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <Notification message={notification} />
+      <Filter value={filter} onChange={handleFilterChange} />
 
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h3>Add a new</h3>
+      <PersonForm
+        onSubmit={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <li key={person.id}>{person.name} {person.number}</li>
-        )}
-      </ul>
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   )
 }
